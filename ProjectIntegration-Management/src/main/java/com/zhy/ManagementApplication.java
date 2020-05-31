5a62966e4bfd9adeb6945a656e3ecc140aebaecc
zZhangYi <1027230413@qq.com>
2020-05-31 18:12:44 +0800

    整合数据库

--- ProjectIntegration-Management/src/main/java/com/zhy/ManagementApplication.java
+++ ProjectIntegration-Management/src/main/java/com/zhy/ManagementApplication.java
@@ -1,9 +1,11 @@
-package zhy;
+package com.zhy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zhy.dao")
 public class ManagementApplication {
 
     public static void main(String[] args) {
--- ProjectIntegration-WEB/src/main/java/com/zhy/WebApplication.java
+++ ProjectIntegration-WEB/src/main/java/com/zhy/WebApplication.java
@@ -1,9 +1,11 @@
 package com.zhy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zhy.dao")
 public class WebApplication {
 
     public static void main(String[] args) {
