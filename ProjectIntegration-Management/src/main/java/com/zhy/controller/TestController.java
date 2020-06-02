013f1d74fb9de4f4e363f9ebc9ee712a3eae33c5
zZhangYi <1027230413@qq.com>
2020-06-02 18:50:31 +0800

    添加zipkin

--- ProjectIntegration-Management/src/main/java/com/zhy/controller/TestController.java
+++ ProjectIntegration-Management/src/main/java/com/zhy/controller/TestController.java
@@ -2,15 +2,20 @@ package com.zhy.controller;
 
 
 import com.zhy.vo.RestfulResultVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @PostMapping("/test")
     public RestfulResultVO test(){
-        return RestfulResultVO.success("success", null);
+        logger.info("mgt运行成功");
+        return RestfulResultVO.success("1234", null);
     }
 
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>  全功能项目骨架</h1>
+  common   所有工具类和常用类模块</br>
+  dao   操作数据库模块</br>
+  eureka  注册中心 </br>
+  management  主要的后台模块</br>
+  vo      实体类模块</br>
+  web     前端交互模块</br>
+  lib     zipkin服务端jar包 java -jar zipkin-server-2.11.8-exec.jar</br>
+          zipkin启动完成后访问http://localhost:9411/zipkin/
